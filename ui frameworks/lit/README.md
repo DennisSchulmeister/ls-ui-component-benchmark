06efa9183170cae5a1d065fcb84ec192b40d5cd6
Dennis Schulmeister-Zimolong <dennis@patk.org>
2024-05-15 14:09:24 +0200

    Rest LIT directory. I confused it with Tonic yesterday

--- ui frameworks/lit/README.md
+++
@@ -1,9 +0,0 @@
-Lit
-===
-
-Commands to initialize the project:
-
-```sh
-npm i lit
-npm add -D esbuild esbuild-plugin-less shelljs
-```
--- ui frameworks/lit/src/index.js
+++
@@ -1,5 +0,0 @@
-import "./index.less";
-
-window.addEventListener("DOMContentLoaded", function() {
-    console.log("Application is ready!");
-});
